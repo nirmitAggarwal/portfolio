273f79966059e7ee19b77a342a4edddaa9b39f1a
nirmit152007 <kiitcommon@gmail.com>
2023-08-17 19:27:19 +0530

    ok uploading my portfolio v1

---
+++ src/app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// creating a database 
+mongoose.connect('mongodb+srv://admin:1234@cluster0.zp0il.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('connected to the mongodb database');
+}).catch((err) => {
+    console.log(err);
+});
+
+
+
+// middleware 
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', path.join(__dirname, '../templates/views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+const Item = mongoose.model('Item', {
+    name: String,
+    email: String,
+    message: String
+});
+
+
+//extras
+var date = new Date();
+var year = date.getFullYear();
+
+var myAge = year - 2007;
+console.log(myAge);
+
+// routing 
+app.get('/', (req, res) => {
+    res.render('index', {
+        age: myAge
+    });
+});
+
+app.post('/contact', (req, res) => {
+    // console.log(req.body);
+    var formReq = req.body;
+    var formSave = new Item(formReq);
+    formSave.save().then(() => {
+        console.log('saved form req');
+        res.redirect('/');
+    });
+
+});
+
+app.get('/cv', (req, res) => {
+    res.render('cv');
+});
+
+
+// listen 
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server is runnig at port')
+});
